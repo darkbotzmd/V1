c81354d2de45a53e6778c27ae82567e869e8f812
darkbotzmd <darkbotzmd@gmail.com>
2023-01-09 01:01:47 +0700

    .

---
+++ options/config.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+API: 'https://zenzapis.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zenzapis.xyz': 'a3db82c2ec',
+}
+
+//━━━━━━━━━━━━━━━[ SETTING ]━━━━━━━━━━━━━━━━━//
+
+global.owner = ['6285870808160']
+global.premium = ['6285870808160']
+global.botname = 'BOT WhatsApp'
+global.packname = 'Dark'
+global.author = 'Dark'
+global.prefa = ['!','.']
+global.sp = '⭔'
+global.mess = {
+success: '✓ Success',
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: 'Loading...',
+endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+premium: "Infinity",
+free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
